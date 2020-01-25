33a37f2663fb81a8b422fba3467707adf02b3400
JunyoungDev <wnsdud2336@naver.com>
2020-01-26 04:00:52 +0900

    API로 영화목록 불러오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
 import PropTypes from "prop-types";
 
 class App extends React.Component{
   state = {
-    count : 0
+    isLoading : true,
+    movies : []
   };
-  add=()=>{
-    this.setState(current => ({count:current.count+1}));
-  };
-  minus=()=>{
-    this.setState(current => ({count:current.count-1}));
-  };
+
+  getMovies = async() => {
+    const {
+      data : {
+        data : {movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({movies : movies, isLoading:false});
+  }
+
+ componentDidMount(){
+   this.getMovies();
+ }
+
   render(){
-  return (
-    <div>
-      <h1>The number is : {this.state.count}</h1>
-      <button onClick={this.add}>Add</button>
-      <button onClick={this.minus}>Minus</button>
-    </div>
-  );
+    const {isLoading, movies} = this.state;
+  return <div>{isLoading ? 
+    "Loading" : 
+    movies.map(movie => {
+      return <Movie 
+        key={movie.id} 
+        id={movie.id} 
+        year={movie.year} 
+        title={movie.title} 
+        summary={movie.summary} 
+        poster={movie.medium_cover_image}/>
+  })}</div>
   }
 }
-
 export default App;
